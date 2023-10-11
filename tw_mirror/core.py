2c3e85e967b009845703c01cc341115ec3bbb410
Rinat Sabitov <rinat.sabitov@gmail.com>
2023-10-11 15:19:26 +0200

    stub for a task source
    Signed-off-by: Rinat Sabitov <rinat.sabitov@gmail.com>

--- tw_mirror/core.py
+++ tw_mirror/core.py
@@ -1,9 +1,17 @@
 import logging
+import subprocess
+import json
 
 logger = logging.getLogger("tw-mirror")
 
 
+def iter_tasks():
+    out = subprocess.check_output(["task", "export"])
+    out = json.loads(out)
+    for item in out:
+        yield item
+
+
 def out_sync(args) -> str:
-    logger.info("test")
-    logger.debug("test")
-    logger.error("test")
+    for task in iter_tasks():
+        print(task)
